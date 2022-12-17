b6c2c96f4a205eaa0a2e9a8e1c89aa91248a355a
Egor <1begosha@gmail.com>
2022-12-17 10:15:48 +0300

    Initial Commit-2

--- Controler.py
+++ Controler.py
@@ -1,15 +1,17 @@
     
 def run():
     value_type = ui.input_value_type
-	mode_work = ui.input_mode
-	log_type(valuetype)
-	log_mode(mode_work)
-	if mode_work==1:
-		expression=exp_to_list(ui.input_digits,value_type)
-	if mode_work==2:
-		expression=exp_to_list(ui.expression,value_type)
+    mode_work = ui.input_mode
+    log_type(valuetype)
+    log_mode(mode_work)
+    if mode_work==1:
+        exp=ui.input_digits
+        expression=exp_to_list(exp,value_type)
+    if mode_work==2:
+        exp=ui.expression
+        expression=exp_to_list(exp,value_type)
 
-	result=calculate(expression)
-	log_exp(expression)
-	log_result(result)
-	print_result(result)
+    result=calculate(expression)
+    log_exp(expression)
+    log_result(result)
+    print_result(exp,result)
